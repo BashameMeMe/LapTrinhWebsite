62fa76a8275e7cd0c33e222369124d078b26baa8
MinhIsMe-2208 <daicahettien2004@gmail.com>
2026-03-22 14:41:11 +0700

    Update view Shipper,Supplier

--- SV22T1020193.Admin/Controllers/Shipper.cs
+++ SV22T1020193.Admin/Controllers/Shipper.cs
@@ -1,24 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SV22T1020193.Models.Common;
 
 namespace SV22T1020193.Admin.Controllers
 {
     public class Shipper : Controller
     {
+        private const string Shipper_search = "ShipperSearchInput";
+
         public IActionResult Index()
         {
-            return View();
+            var input = ApplicationContext.GetSessionData<PaginationSearchInput>(Shipper_search);
+            if (input == null)
+                input = new PaginationSearchInput()
+                {
+                    Page = 1,
+                    PageSize = 5,
+                    SearchValue = ""
+                };
+
+            return View(input);
         }
+
+        public async Task<IActionResult> Search(PaginationSearchInput input)
+        {
+            var result = await PartnerDataService.ListShippersAsync(input);
+            ApplicationContext.SetSessionData(Shipper_search, input);
+
+            return PartialView("Search", result); // AJAX
+        }
+
         public IActionResult Create()
         {
-            return View();
+            ViewBag.Title = "Bổ sung người giao hàng";
+            return View("Edit");
         }
-        public IActionResult Edit(int id)
+
+        public async Task<IActionResult> Edit(int id)
         {
-            return View();
+            ViewBag.Title = "Cập nhật người giao hàng";
+            var data = await PartnerDataService.GetShipperAsync(id);
+            return View(data);
         }
-        public IActionResult Delete(int id)
+
+        public async Task<IActionResult> Delete(int id)
         {
-            return View();
+            ViewBag.Title = "Xóa người giao hàng";
+            var data = await PartnerDataService.GetShipperAsync(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id, IFormCollection form)
+        {
+            await PartnerDataService.DeleteShipperAsync(id);
+            return RedirectToAction("Index");
         }
     }
 }
--- SV22T1020193.Admin/Controllers/Supplier.cs
+++ SV22T1020193.Admin/Controllers/Supplier.cs
@@ -1,44 +1,81 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SV22T1020193.Models.Common;
 
 namespace SV22T1020193.Admin.Controllers
 {
-    /// <summary>
-    /// Cac chuc nang lien quan den nha cung cap
-    /// </summary>
     public class Supplier : Controller
-    {/// <summary>
-    /// TIm kiem va hien thi danh sach nha cung cap
-    /// </summary>
-    /// <returns></returns>
+    {
+        /// <summary>
+        /// Lưu điều kiện tìm kiếm nhà cung cấp trong session
+        /// </summary>
+        private const string Supplier_search = "SupplierSearchInput";
+
+        /// <summary>
+        /// Nhập đầu vào tìm kiếm, hiển thị kết quả tìm kiếm
+        /// </summary>
         public IActionResult Index()
         {
-            return View();
+            var input = ApplicationContext.GetSessionData<PaginationSearchInput>(Supplier_search);
+            if (input == null)
+                input = new PaginationSearchInput()
+                {
+                    Page = 1,
+                    PageSize = 5,
+                    SearchValue = ""
+                };
+
+            return View(input);
         }
+
         /// <summary>
-        /// Bo sung nha cung cap moi
+        /// Tìm kiếm và trả về kết quả
         /// </summary>
-        /// <returns></returns>
+        public async Task<IActionResult> Search(PaginationSearchInput input)
+        {
+            var result = await PartnerDataService.ListSuppliersAsync(input);
+            ApplicationContext.SetSessionData(Supplier_search, input);
+            return View(result);
+        }
+
+        /// <summary>
+        /// Bổ sung nhà cung cấp
+        /// </summary>
         public IActionResult Create()
         {
             ViewBag.Title = "Bổ sung nhà cung cấp";
             return View("Edit");
-        }/// <summary>
-        /// Chinh sua,cap nhat nha cung 
+        }
+
+        /// <summary>
+        /// Cập nhật nhà cung cấp
         /// </summary>
-        /// <param name="id">Ma nha cap cap can cap nhat</param>
-        /// <returns></returns>
-        public IActionResult Edit(int id)
+        public async Task<IActionResult> Edit(int id)
         {
             ViewBag.Title = "Cập nhật nhà cung cấp";
-            return View();
-        }/// <summary>
-        /// Xoa nha cung cap
+
+            var data = await PartnerDataService.GetSupplierAsync(id);
+            return View(data);
+        }
+
+        /// <summary>
+        /// Xóa nhà cung cấp (GET)
         /// </summary>
-        /// <param name="id">Ma nha cung cap can xoa</param>
-        /// <returns></returns>
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            return View();
+            ViewBag.Title = "Xóa nhà cung cấp";
+
+            var data = await PartnerDataService.GetSupplierAsync(id);
+            return View(data);
+        }
+
+        /// <summary>
+        /// Xóa nhà cung cấp (POST)
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id, IFormCollection form)
+        {
+            await PartnerDataService.DeleteSupplierAsync(id);
+            return RedirectToAction("Index");
         }
     }
 }
